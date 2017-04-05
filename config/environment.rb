0edebbe9e08c58cb564169b21bdd47acb11cd633
Rails test course <railscoursetest@gmail.com>
2017-04-05 19:03:19 +0000

    mailer-sender edited

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
-    :port => '587',
+    :port => '2525',
     :authentication => :plain, 
     :username => ENV['SENDGRID_USERNAME'], 
     :password => ENV['SENDGRID_PASSWORD'],
